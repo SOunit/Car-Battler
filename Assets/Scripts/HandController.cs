c508e28d9f103478159bebd8ff276b9e3ffa2249
SOUnit <workbookwork7@gmail.com>
2022-11-07 12:18:09 -0800

    add min, max hand positions

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    public Card[] heldCards;
+
+    public Transform
+
+            minPos,
+            maxPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCardPositionsInHand();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetCardPositionsInHand()
+    {
+    }
+}
