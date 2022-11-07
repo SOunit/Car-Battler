90d6a23d33dece86d4f48b215d6f66d1b0d40349
SOUnit <workbookwork7@gmail.com>
2022-11-07 12:50:46 -0800

    set card positions in hands

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
-    public Card[] heldCards;
+    public List<Card> heldCards = new List<Card>();
 
     public Transform
 
             minPos,
             maxPos;
+
+    public List<Vector3> cardPositions = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +26,20 @@ public class HandController : MonoBehaviour
 
     public void SetCardPositionsInHand()
     {
+        cardPositions.Clear();
+
+        Vector3 distanceBetweenPoints = Vector3.zero;
+        if (heldCards.Count > 1)
+        {
+            distanceBetweenPoints =
+                (maxPos.position - minPos.position) / (heldCards.Count - 1);
+        }
+
+        for (int i = 0; i < heldCards.Count; i++)
+        {
+            cardPositions.Add(minPos.position + (distanceBetweenPoints * i));
+
+            heldCards[i].transform.position = cardPositions[i];
+        }
     }
 }
