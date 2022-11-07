8378a78f3ca796a9f99ab78111e83a681951343b
SOUnit <workbookwork7@gmail.com>
2022-11-07 07:43:49 -0800

    add image of card to scriptable-object

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public CardScriptableObject cardSO;
+
     public int currentHealth;
 
     public int
@@ -16,14 +19,38 @@ public class Card : MonoBehaviour
 
             healthText,
             attackText,
-            manaCostText;
+            manaCostText,
+            nameText,
+            actionDescriptionText,
+            loreText;
+
+    public Image
+
+            characterArt,
+            bgArt;
 
     // Start is called before the first frame update
     void Start()
     {
+        SetupCard();
+    }
+
+    public void SetupCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
+
         healthText.text = currentHealth.ToString();
         attackText.text = attackPower.ToString();
         manaCostText.text = manaCost.ToString();
+
+        nameText.text = cardSO.cardName;
+        actionDescriptionText.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.characterSprite;
+        bgArt.sprite = cardSO.bgSprite;
     }
 
     // Update is called once per frame
