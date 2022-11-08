4a55c175d2030582651f084f92a38b3f323a9193
SOUnit <workbookwork7@gmail.com>
2022-11-08 13:00:20 -0800

    add setup deck logic

---
+++ Assets/Scripts/DeckController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckController : MonoBehaviour
+{
+    public static DeckController instance;
+
+    public List<CardScriptableObject>
+        deckToUse = new List<CardScriptableObject>();
+
+    private List<CardScriptableObject>
+        activeCards = new List<CardScriptableObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupDeck();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetupDeck()
+    {
+        activeCards.Clear();
+
+        List<CardScriptableObject> tempDeck = new List<CardScriptableObject>();
+        tempDeck.AddRange (deckToUse);
+
+        int iteration = 0;
+        while (tempDeck.Count > 0 && iteration < 500)
+        {
+            int selected = Random.Range(0, tempDeck.Count);
+            activeCards.Add(tempDeck[selected]);
+            tempDeck.RemoveAt (selected);
+
+            iteration++;
+        }
+    }
+}
