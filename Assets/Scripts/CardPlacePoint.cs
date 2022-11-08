193b1937018e9a25e9e0162199fc13c6ccf77cea
SOUnit <workbookwork7@gmail.com>
2022-11-07 22:36:46 -0800

    add enemy-card-places

---
+++ Assets/Scripts/CardPlacePoint.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPlacePoint : MonoBehaviour
+{
+    public Card activeCard;
+
+    public bool isPlayerPoint;
+}
